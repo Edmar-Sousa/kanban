169ada9ba7f3acdbc5cc31ab377a7b276fe40360
edinho <edmarsousamendes4@gmail.com>
2023-12-17 10:40:53 -0300

    feat: use modal component

--- resources/js/Pages/Team.vue
+++ resources/js/Pages/Team.vue
@@ -21,10 +21,18 @@
             <div class="my-10">
                 <button 
                     type="button" 
-                    class="w-[200px] rounded-md bg-[#7C3AED] flex items-center gap-2 justify-center h-10 text-white text-base text-center"
-                    @click="openModal = true">
+                    class="w-[200px] rounded-md bg-[#7C3AED] inline-flex items-center gap-2 justify-center h-10 text-white text-base text-center"
+                    @click="handleOpenModal">
                         <user-plus-2 size="20" />
                         <span>Convidar</span>
+                </button>
+
+                <button 
+                    type="button" 
+                    class="w-[200px] rounded-md text-[#7C3AED] bg-[#7C3AED]/30 inline-flex items-center gap-2 justify-center h-10 text-base text-center ml-4"
+                    >
+                        <user-plus-2 size="20" />
+                        <span>Convites</span>
                 </button>
             </div>
 
@@ -33,35 +41,15 @@
                     <div class="w-10 h-10 rounded bg-[#7C3AED] text-white leading-10 text-xl text-center uppercase">{{ letter }}</div>
     
                     <ul class="mt-5">
-                        <li class="px-10 mt-4 flex justify-between flex-col md:items-center md:flex-row" v-for="( friend, i ) in filterByLetter( letter )" :key="i">
-                            <div class="flex items-center mb-4">
-                                <img 
-                                    :src="loadImage( friend.source_user_data.image )" 
-                                    alt="imagem de perfil"
-                                    class="w-16 h-16 mx-7 rounded-full" />
-                                
-                                <div>
-                                    <p class="font-bold text-xl">{{ friend.source_user_data.name }}</p>
-                                    <p class="text-base text-[#1E293B]">{{ friend.source_user_data.email }}</p>
-                                </div>
-                            </div>
-    
-                            <div class="flex flex-wrap justify-end gap-4">
-                                <template v-if="friend.status == 3">
-                                    <button
-                                        type="button"
-                                        class="inline-block w-[150px] rounded-md bg-[#7C3AED] px-4 items-center gap-2 justify-center h-10 text-white text-sm text-center hover:scale-95"
-                                        @click="handleAcceptInvite(friend.id)">
-                                            Aceitar convite
-                                    </button>
-    
-                                    <button
-                                        type="button"
-                                        class="inline-block w-[150px] rounded-md bg-[#7C3AED] px-4 items-center gap-2 justify-center h-10 text-white text-sm text-center hover:scale-95">
-                                            Recusar convite
-                                    </button>
-                                </template>
-    
+                        <li class="px-10 mt-4 flex items-center" v-for="( friend, i ) in filterByLetter( letter )" :key="i">
+                            <img 
+                                :src="loadImage( friend.source_user_data.image )" 
+                                alt="imagem de perfil"
+                                class="w-16 h-16 mx-7 rounded-full" />
+                            
+                            <div>
+                                <p class="font-bold text-xl">{{ friend.source_user_data.name }}</p>
+                                <p class="text-base text-[#1E293B]">{{ friend.source_user_data.email }}</p>
                             </div>
                         </li>
                     </ul>
@@ -70,79 +58,64 @@
         </main>
     </Layout>
 
-    <div class="fixed top-0 bottom-0 left-0 right-0 bg-[#00000033] flex justify-center items-center" v-show="openModal">
-      <div class="bg-white p-4 rounded w-full max-w-[500px]">
+    <modal 
+        ref="modalSendInvite" 
+        title="Enviar convite">
+            <template #modal-body>
+                <div 
+                    v-show="messageresponse.status.length"
+                    class="w-full p-4 mt-3 text-lg font-bold border rounded" 
+                    :class="messagestyle">
+                        <p>
+                            {{ messageresponse.message }}
+                        </p>
+                </div>
 
-        <div class="w-full flex justify-between">
-          <h2 class="font-bold">Enviar convite</h2>
+                <form action="#" method="POST" @submit.prevent>
+                    <div class="my-4">
+                        <InputForm
+                            type="email"
+                            name="username"
+                            placeholder="Email do usuario"
+                            v-model="email" />
+                    </div>
 
-          <button arial-label="Close modal" @click="openModal = false">
-            <X size="20" />
-          </button>
-        </div>
+                    <button 
+                        type="button" 
+                        class="w-[200px] float-right rounded-md bg-[#7C3AED] flex items-center gap-2 justify-center h-10 text-white text-base text-center"
+                        @click="submitForm">
+                            <user-plus-2 size="20" />
+                            <span>Convidar</span>
+                    </button>
+                </form>
+            </template>
+    </modal>
 
-        <div 
-            v-show="messageresponse.status.length"
-            class="w-full p-4 mt-3 text-lg font-bold border rounded" 
-            :class="{ 
-                'border-green-700 text-green-700 bg-green-400': messageresponse.status == 'success',
-                'border-red-700 text-red-700 bg-red-400': messageresponse.status == 'error',
-            }"
-        >
-            <p>{{ messageresponse.message }}</p>
-        </div>
-
-        <form action="#" method="POST" @submit.prevent>
-            <div class="my-4">
-                <InputForm
-                    type="email"
-                    name="username"
-                    placeholder="Email do usuario"
-                    v-model="email" />
-            </div>
-
-            <button 
-                type="button" 
-                class="w-[200px] float-right rounded-md bg-[#7C3AED] flex items-center gap-2 justify-center h-10 text-white text-base text-center"
-                @click="submitForm">
-                    <user-plus-2 size="20" />
-                    <span>Convidar</span>
-            </button>
-        </form>
-        
-      </div>
-    </div>
 </template>
 
 
 <script setup>
 
-import { UserPlus2, X } from 'lucide-vue-next'
+import axios from 'axios'
 
-import Layout from '../Template/Layout.vue'
+import { UserPlus2 } from 'lucide-vue-next'
 import { computed, shallowRef, ref, onMounted } from 'vue'
 
+
+import Layout from '../Template/Layout.vue'
 import InputForm from '../Components/InputForm.vue'
 import Notification from '../Components/Notification.vue'
+import Modal from '../Components/Modal.vue'
 
 import websocket from '../Utils/websocket'
 import jwttoken from '../Utils/jwttoken'
-import axios from 'axios'
 
 const props = defineProps( [ 'image', 'friends' ] )
 
-const openModal = shallowRef( false )
-const email = shallowRef('')
-
-const messageresponse = ref({
-    status: '',
-    message: '',
-})
 
 const friendsList = ref([])
 
 const getFirstLetterFriendsList = computed( () => new Set( friendsList.value.map( friend => friend.source_user_data.name.charAt(0) ) ) )
-
 
 onMounted(() => {
     websocket.connect(jwttoken.getToken())
@@ -155,6 +128,17 @@ function filterByLetter( letter ) {
     return friendsList.value.filter( friend => friend.source_user_data.name.charAt(0) == letter )
 }
 
+
+const email = shallowRef('')
+const messageresponse = ref({
+    status: '',
+    message: '',
+})
+
+const messagestyle = computed(() => messageresponse.value.status == 'success' 
+    ? 'border-green-700 text-green-700 bg-green-400'
+    : 'border-red-700 text-red-700 bg-red-400'
+)
 
 async function submitForm() {
     websocket.send({
@@ -185,6 +169,14 @@ function handleAcceptInvite(id) {
     websocket.recv('accept-invite-success', data => console.log(data))
     websocket.recv('accept-invite-error', data => console.log(data))
 
+}
+
+
+const modalSendInvite = ref(null)
+
+function handleOpenModal() {
+    if (modalSendInvite.value)
+        modalSendInvite.value.showModal()
 }
 
 
