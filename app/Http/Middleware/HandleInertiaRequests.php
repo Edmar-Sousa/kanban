3c76c63db00c52382da2071cdf4d6a2050edfb89
Edmar-Sousa <edmarsousamendes4@gmail.com>
2024-11-09 18:11:12 -0300

    feat: middleware to shared data

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,7 +36,10 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            //
+            'flash' => fn() => [
+                'status' => $request->session()->get('status'),
+                'message' => $request->session()->get('message'),
+            ],
         ]);
     }
 }
