3769e401f1eb260ee268058e8a09a088d7892784
Edmar-Sousa <edmarsousamendes4@gmail.com>
2024-11-09 15:36:49 -0300

    fix: change import to useForm

--- resources/js/Pages/Config.vue
+++ resources/js/Pages/Config.vue
@@ -166,7 +166,7 @@
 <script setup>
 
 import { ref } from 'vue'
-import { useForm } from '@inertiajs/inertia-vue3'
+import { useForm } from '@inertiajs/vue3'
 import { useToast } from 'vue-toast-notification'
 
 import { useVuelidate } from '@vuelidate/core'
