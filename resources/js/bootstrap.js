a44638ee0d4e49e463885328b513ba00f965ba68
Edmar-Sousa <edmarsousamendes4@gmail.com>
2024-11-10 18:09:11 -0300

    feat: config to laravel reverb

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,12 @@
-import axios from 'axios';
-window.axios = axios;
+import axios from 'axios'
+window.axios = axios
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
+
+import './echo'
