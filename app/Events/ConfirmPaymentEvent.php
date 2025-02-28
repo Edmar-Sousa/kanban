d43046f13407c614494bb35f8130853d7f6df627
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-02-27 22:01:21 -0300

    fix: send confirm payment with reverb

--- app/Events/ConfirmPaymentEvent.php
+++ app/Events/ConfirmPaymentEvent.php
@@ -2,6 +2,8 @@
 
 namespace App\Events;
 
+use App\Models\Transaction;
+
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
@@ -9,28 +11,25 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
-class ConfirmPaymentEvent
+class ConfirmPaymentEvent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    /**
-     * Create a new event instance.
-     */
-    public function __construct()
+    public string $paymentId;
+    public string $status;
+
+
+    public function __construct(Transaction $transaction)
     {
-        //
+        $this->paymentId = $transaction->id;
+        $this->status = $transaction->status;
     }
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
-     */
-    public function broadcastOn(): array
+
+    public function broadcastOn(): Channel
     {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
+        return new PrivateChannel('payment.' . $this->paymentId);
     }
 }
