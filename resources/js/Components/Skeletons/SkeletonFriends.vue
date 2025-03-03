3969626f6d89c473d97f3d4ea125dec116b1530c
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-03-02 22:02:12 -0300

    feat: component skeleton loading to contacts

---
+++ resources/js/Components/Skeletons/SkeletonFriends.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+defineProps({
+    numberContacts: {
+        type: Number,
+        required: false,
+        default: 5,
+    }
+})
+
+</script>
+
+<template>
+    <div class="mt-6">
+        <div class="w-10 h-10 rounded bg-gray-300 leading-10 text-xl text-center uppercase skeleton-loading"></div>
+
+        <div
+            v-for="contact in numberContacts"
+            :key="contact"
+            class="mt-5 px-10 flex items-center">
+                <div class="w-16 h-16 mx-7 bg-gray-300 rounded-full skeleton-loading"></div>
+                <div>
+                    <div class="w-[100px] h-6 rounded bg-gray-300 skeleton-loading"></div>
+                    <div class="w-[200px] h-4 rounded mt-2 bg-gray-300 skeleton-loading"></div>
+                </div>
+        </div>
+    </div>
+
+</template>
+
