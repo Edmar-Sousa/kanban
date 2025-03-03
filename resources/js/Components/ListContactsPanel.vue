e1b02cf2a357ea897a381c39d78e939d2cc8bc05
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-03-02 22:03:35 -0300

    refact: stract list contacts to component list contacts

---
+++ resources/js/Components/ListContactsPanel.vue
@@ -0,0 +1,133 @@
+<script setup>
+
+import axios from "axios"
+
+import {UserX} from "lucide-vue-next"
+import {useToast} from "vue-toast-notification"
+import {computed, ref} from "vue"
+
+
+const props = defineProps({
+    id: {
+        type: Number,
+        required: true,
+    }
+})
+
+const toast = useToast({
+    position: 'top-right',
+})
+
+const friendsList = ref([])
+findFriends()
+async function findFriends() {
+    try {
+        const response = await axios.get(route('team.list'))
+        friendsList.value = response.data
+    }
+
+    catch {
+        toast.error('Erro ao fazer convite, tente mais tarde')
+    }
+}
+
+function filterByLetter( letter ) {
+    return friendsList.value.filter( friend => {
+        const firstLetter = friend.source_user_data.id === props.id  ?
+            friend.destination_user_data.name.charAt(0) :
+            friend.source_user_data.name.charAt(0)
+
+
+        return firstLetter === letter
+    })
+}
+
+const getFirstLetterFriendsList = computed( () => new Set(
+        friendsList.value.map(friend => friend.source_user_data.id === props.id ?
+            friend.destination_user_data.name.charAt(0) :
+            friend.source_user_data.name.charAt(0)
+        )
+    )
+)
+
+
+async function handleDeleteFriend(id) {
+    try {
+        const response = await axios.delete(route('team.delete', { id }))
+
+        if (response.status === 200)
+        {
+            toast.success(response.data?.message)
+            findFriends()
+        }
+    }
+
+    catch {
+        toast.error('Erro ao remover da lista de amigos')
+    }
+}
+
+</script>
+
+<template>
+
+    <template v-if="friendsList.length">
+        <div v-for="letter in getFirstLetterFriendsList" :key="letter" class="my-6">
+            <div class="w-10 h-10 rounded bg-[#7C3AED] text-white leading-10 text-xl text-center uppercase">{{ letter }}</div>
+
+            <ul class="mt-5">
+                <li class="px-10 mt-4 flex items-center" v-for="( friend, i ) in filterByLetter( letter )" :key="i">
+                    <img
+                        :src="loadImage(
+                            friend.source_user_data.id == id ?
+                            friend.destination_user_data.image :
+                            friend.source_user_data.image
+                        )"
+                        alt="imagem de perfil"
+                        class="w-16 h-16 mx-7 rounded-full" />
+
+                    <div class="flex-1">
+                        <p class="font-bold text-xl">
+                            {{
+                                friend.source_user_data.id == id ?
+                                    friend.destination_user_data.name :
+                                    friend.source_user_data.name
+                            }}
+                        </p>
+                        <p class="text-base text-[#1E293B]">
+                            {{
+                                friend.source_user_data.id == id ?
+                                    friend.destination_user_data.email :
+                                    friend.source_user_data.email
+                            }}
+                        </p>
+                    </div>
+
+                    <div>
+                        <button
+                            type="button"
+                            class="w-9 h-9 bg-red-500/50 text-red-700 leading-10 rounded-md hover:scale-95"
+                            @click="handleDeleteFriend( friend.source_user_data.id == id ? friend.destination_user_data.id : friend.source_user_data.id)">
+                                <user-x size="20" class="mx-auto" />
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </template>
+
+<!--    <div v-else class="mt-10">-->
+<!--        <h2 class="text-xl font-bold text-center mb-1 text-[#403937]">-->
+<!--            Nenhum amigo encontrado-->
+<!--        </h2>-->
+
+<!--        <p class="text-center text-base mb-8 text-[#403937]">-->
+<!--            Adicione amigos para interagir com suas tarrefas-->
+<!--        </p>-->
+
+<!--        <img-->
+<!--            src="/images/undraw_social_friends.svg"-->
+<!--            class="w-1/2 mx-auto max-w-[500px]" />-->
+<!--    </div>-->
+
+</template>
--- resources/js/Pages/Team.vue
+++ resources/js/Pages/Team.vue
@@ -36,65 +36,15 @@
                 </button>
             </div>
 
-            <template v-if="friendsList.length">
-                <div v-for="letter in getFirstLetterFriendsList" :key="letter" class="my-6">
-                    <div class="w-10 h-10 rounded bg-[#7C3AED] text-white leading-10 text-xl text-center uppercase">{{ letter }}</div>
+            <Suspense>
+                <template #default>
+                    <AsyncListContactsPanel :id="id" />
+                </template>
 
-                    <ul class="mt-5">
-                        <li class="px-10 mt-4 flex items-center" v-for="( friend, i ) in filterByLetter( letter )" :key="i">
-                            <img
-                                :src="loadImage(
-                                    friend.source_user_data.id == id ?
-                                    friend.destination_user_data.image :
-                                    friend.source_user_data.image
-                                )"
-                                alt="imagem de perfil"
-                                class="w-16 h-16 mx-7 rounded-full" />
-
-                            <div class="flex-1">
-                                <p class="font-bold text-xl">
-                                    {{
-                                        friend.source_user_data.id == id ?
-                                        friend.destination_user_data.name :
-                                        friend.source_user_data.name
-                                    }}
-                                </p>
-                                <p class="text-base text-[#1E293B]">
-                                    {{
-                                        friend.source_user_data.id == id ?
-                                        friend.destination_user_data.email :
-                                        friend.source_user_data.email
-                                    }}
-                                </p>
-                            </div>
-
-                            <div>
-                                <button
-                                    type="button"
-                                    class="w-9 h-9 bg-red-500/50 text-red-700 leading-10 rounded-md hover:scale-95"
-                                    @click="handleDeleteFriend( friend.source_user_data.id == id ? friend.destination_user_data.id : friend.source_user_data.id)">
-                                        <user-x size="20" class="mx-auto" />
-                                </button>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </template>
-
-            <div v-else class="mt-10">
-                <h2 class="text-xl font-bold text-center mb-1 text-[#403937]">
-                    Nenhum amigo encontrado
-                </h2>
-
-                <p class="text-center text-base mb-8 text-[#403937]">
-                    Adicione amigos para interagir com suas tarrefas
-                </p>
-
-                <img
-                    src="/images/undraw_social_friends.svg"
-                    class="w-1/2 mx-auto max-w-[500px]" />
-
-            </div>
+                <template #fallback>
+                    <SkeletonFriends />
+                </template>
+            </Suspense>
         </main>
     </Layout>
 
@@ -117,7 +67,7 @@
                         type="button"
                         class="w-[200px] float-right rounded-md bg-[#7C3AED] flex items-center gap-2 justify-center h-10 text-white text-base text-center"
                         @click="submitForm">
-                            <user-plus-2 size="20" />
+                            <UserPlus2 size="20" />
                             <span>Convidar</span>
                     </button>
                 </form>
@@ -181,44 +131,24 @@
 
 import axios from 'axios'
 
-import { UserPlus2, UserCheck, UserX } from 'lucide-vue-next'
-import { computed, shallowRef, ref, onMounted } from 'vue'
+import { UserCheck, UserPlus2, UserX } from 'lucide-vue-next'
+import { shallowRef, ref, defineAsyncComponent} from 'vue'
 import { useToast } from 'vue-toast-notification'
 
 import Layout from '@/Template/Layout.vue'
 import InputForm from '@/Components/InputForm.vue'
 import Notification from '@/Components/Notification.vue'
+import SkeletonFriends from "@/Components/Skeletons/SkeletonFriends.vue"
+
 import Modal from '@/Components/Modal.vue'
 import { useForm } from '@inertiajs/vue3'
 
 
 const props = defineProps( [ 'image', 'id' ] )
 
-
-const friendsList = ref([])
-
-const getFirstLetterFriendsList = computed( () => new Set( friendsList.value.map( friend => {
-    return friend.source_user_data.id == props.id       ?
-           friend.destination_user_data.name.charAt(0)  :
-           friend.source_user_data.name.charAt(0)
-} ) ) )
-
-onMounted(() => {
-    findFrinds()
+const AsyncListContactsPanel = defineAsyncComponent({
+    loader: () => import('@/Components/ListContactsPanel.vue'),
 })
-
-
-function filterByLetter( letter ) {
-    return friendsList.value.filter( friend => {
-        const firstLetter = friend.source_user_data.id == props.id  ?
-           friend.destination_user_data.name.charAt(0) :
-           friend.source_user_data.name.charAt(0)
-
-
-        return firstLetter == letter
-    })
-}
-
 
 const inviteForm = useForm({
     email: '',
@@ -231,19 +161,7 @@ function clearMessages() {
 }
 
 async function submitForm() {
-
     inviteForm.post(route('team.invite.create'))
-
-    // websocket.send({
-    //     event: 'invite-friend',
-    //     token: jwttoken.getToken(),
-    //     data: {
-    //         email: email.value,
-    //     }
-    // })
-
-    // websocket.recv('invite-friend-success', data => messageresponse.value = data)
-    // websocket.recv('invite-friend-error', data => messageresponse.value = data)
 }
 
 const invitesData = ref(null)
@@ -294,36 +212,6 @@ async function handleViewInvites() {
     }
     catch {
         toast.error('Erro ao buscar convites, tente mais tarde')
-    }
-}
-
-
-async function findFrinds() {
-    try {
-        const response = await axios.get(route('team.list'))
-
-        friendsList.value = response.data
-    }
-
-    catch {
-        toast.error('Erro ao fazer convite, tente mais tarde')
-    }
-}
-
-
-async function handleDeleteFriend(id) {
-    try {
-        const response = await axios.delete(route('team.delete', { id }))
-
-        if (response.status === 200)
-        {
-            toast.success(response.data?.message)
-            findFrinds()
-        }
-    }
-
-    catch {
-        toast.error('Erro ao remover da lista de amigos')
     }
 }
 
