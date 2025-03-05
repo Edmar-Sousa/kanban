8e26d01fa3570bca0799769d37fc9196e39c5db5
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-03-05 20:31:19 -0300

    refact: use the Storage class to save the temp file

--- app/Logic/ProcessFile.php
+++ app/Logic/ProcessFile.php
@@ -7,12 +7,13 @@ use App\Jobs\UploadImageJob;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class ProcessFile
 {
 
-    public static function processUploadFile(Request &$request, $user)
+    public static function processUploadFile(Request &$request, $user): void
     {
 
         $file = $request->file('image');
@@ -20,13 +21,13 @@ class ProcessFile
         $file_extension = $file->getClientOriginalExtension();
         $file_name = Str::random(35) . time();
 
-        $store_path = storage_path('app/jobs') . '/' . $file_name . '.' . $file_extension;
+        $relative_path = 'jobs/' . $file_name . '.' . $file_extension;
 
-        move_uploaded_file($file->getRealPath(), $store_path);
+        Storage::put($relative_path, file_get_contents($file->getRealPath()));
+        $storage_path = Storage::path($relative_path);
 
-        UploadImageJob::dispatch($user->id, $store_path, $file_name);
-
+        UploadImageJob::dispatch($user->id, $storage_path, $file_name);
     }
-    
+
 }
 
