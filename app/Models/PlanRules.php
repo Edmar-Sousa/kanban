1af6d9580bbc163581193d2bc2478d1b14036e8b
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-03-05 11:07:03 -0300

    feat: check if the rules is premium plan

--- app/Models/PlanRules.php
+++ app/Models/PlanRules.php
@@ -10,4 +10,11 @@ class PlanRules extends Model
     use HasFactory;
 
     protected $fillable = [ 'limit_boards', 'limit_users', 'limit_reports', 'plan_id' ];
+
+
+    public function getIsPremiumAttribute()
+    {
+        return $this->limit_boards == 0 && $this->limit_users == 0 && $this->limit_reports == 0;
+    }
+
 }
