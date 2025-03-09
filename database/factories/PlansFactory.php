a832cc1c1f5bb8715cbec97d7bc21f622540e0d5
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-03-09 15:36:23 -0300

    feat: factory to plans

---
+++ database/factories/PlansFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Plans>
+ */
+class PlansFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->words(3),
+            'small_description' => $this->faker->words(5),
+            'price' => $this->faker->randomFloat(2, 10, 100),
+        ];
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -37,9 +37,6 @@ class UserFactory extends Factory
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
