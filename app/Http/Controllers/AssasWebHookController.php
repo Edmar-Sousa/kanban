a3ad1e8a5d62f8ace36bdc0e5da784ec705580f7
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-19 23:19:26 -0300

    fix: add return type declaration to __invoke method in AssasWebHookController

--- app/Http/Controllers/AssasWebHookController.php
+++ app/Http/Controllers/AssasWebHookController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AssasWebHookController extends Controller
 {
 
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): Response
     {
         return response('', 200);
     }
