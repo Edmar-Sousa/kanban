c026837dfdaf92e64790c9b9ed5086dff6851d85
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-04-20 16:49:18 -0300

    fix: correct formatting and add missing docblocks in CustomException class

--- app/Exceptions/CustomException.php
+++ app/Exceptions/CustomException.php
@@ -9,9 +9,19 @@ class CustomException extends Exception
 {
 
     private string $errorId;
+
+    /** @var array<string, string> */
     private array $messages;
 
 
+    /**
+     * Class to reprer the custom exception
+     * 
+     * @param string $errorId
+     * @param int $statusCode
+     * @param string $message
+     * @param array<string, string> $messages 
+     */
     public function __construct(string $errorId, int $statusCode, string $message, array $messages = [])
     {
         parent::__construct($message, $statusCode);
@@ -22,6 +32,11 @@ class CustomException extends Exception
 
 
 
+    /**
+     * Function to return an array from error
+     * 
+     * @return array{code: string, errors: array<string, string>, status: int}
+     */
     public final function errorObject(): array
     {
         return [
