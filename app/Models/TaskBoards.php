efcb0b2f516f880eea3f18069e3dc2a2df8cb749
Edmar-Sousa <edmarsousamendes4@gmail.com>
2025-07-20 01:47:13 -0300

    fix: try fixing php stan errors

--- app/Models/TaskBoards.php
+++ app/Models/TaskBoards.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+
+/**
+ * @property-read int $id
+ * 
+ * @property string $title
+ * @property string $description
+ * @property int $user_id
+ */
 class TaskBoards extends Model
 {
     use HasFactory;
@@ -15,9 +25,9 @@ class TaskBoards extends Model
     /**
      *   This function return the TaskBoards with task
      * 
-     *   @return Illuminate\Database\Eloquent\Collection[]  Return a collection from tasks
+     *   @return HasMany<Task, TaskBoards>
      */
-    public function tasks()
+    public function tasks(): HasMany
     {
         return $this->hasMany(Task::class, 'taskboard_id', 'id');
     }
@@ -27,10 +37,8 @@ class TaskBoards extends Model
      *   This function return the taskboard with id and task from taskboard
      * 
      *   @param int $id  The is of task board to get
-     * 
-     *   @return Illuminate\Database\Eloquent\Model  Return the taskboard data
      */
-    public function get_task_board_with_tasks( int $id )
+    public function get_task_board_with_tasks( int $id ): TaskBoards | null
     {
         return $this->where('id', $id)
                     ->with('tasks')
@@ -55,9 +63,9 @@ class TaskBoards extends Model
      * 
      *   @param int $user_id  The user id to find taskboard
      * 
-     *   @return Illuminate\Database\Eloquent\Collection[]  Return array of collections with taskboard data
+     *   @return Collection<int, \App\Models\TaskBoards>  Return array of collections with taskboard data
      */
-    public function get_task_board_user(int $user_id)
+    public function get_task_board_user(int $user_id): Collection
     {
         return $this->where('user_id', $user_id)
                     ->get();
@@ -67,14 +75,10 @@ class TaskBoards extends Model
     /**
      *   This function will create a new register of taskboard in database
      * 
-     *   @param array $task_board_data  The data of task board to register
-     *   @param int $user_id  The id of user that create task board
-     * 
-     *   @return Illuminate\Database\Eloquent\Model  Return a model with data of new register
-     * 
-     *   @throws Illuminate\Database\QueryException  Return an exception in case of failure
+     *   @param array{ title: string, description: string } $task_board_data
+     *   @param int $user_id
      */
-    public function create_taskboard( array $task_board_data, int $user_id )
+    public function create_taskboard( array $task_board_data, int $user_id ): TaskBoards
     {
         return $this->create([
             'user_id' => $user_id,
@@ -88,10 +92,11 @@ class TaskBoards extends Model
      *   This function will delete the register of taskboard with id
      * 
      *   @param int $id  The id of taskboard to delete
+     * 
+     *   @return bool
      */
-    public function delete_taskboard( int $id )
+    public function delete_taskboard( int $id ): bool
     {
-        return $this->where('id', $id)
-                    ->delete();
+        return $this->where('id', $id)->delete() > 0;
     }
 }
